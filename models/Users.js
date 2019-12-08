9df2164a2371ac1ddcc7ae7b668a63fa1d4a7935
John <doanja@plu.edu>
2019-12-07 23:36:01 -0800

    added models for user, profile, and banking account

---
+++ models/Users.js
@@ -0,0 +1,36 @@
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [1, 20] }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [1, 20] }
+    }
+  });
+
+  User.associate = models => {
+    User.belongsTo(models.Profile, {
+      foreignKey: {
+        fieldName: "profileId",
+        defaultValue: null
+      }
+    });
+  };
+
+  User.associate = models => {
+    User.hasMany(models.BankingAccount, {
+      onDelete: "cascade"
+    });
+  };
+
+  return User;
+};
