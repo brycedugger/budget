ca0841d323143f84ac43464d903f27c426de6247
JC Lenz <jc@miserablepop.com>
2019-12-13 15:59:34 -0800

    Created controllers folder and authcontroller.js. Commented out the code within authcontroller for use later

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,23 +1,23 @@
-var exports = (module.exports = {});
+// var exports = (module.exports = {});
 
-// For sign up
-exports.signup = function(req, res) {
-  res.render("signup");
-};
+// // For sign up
+// exports.signup = function(req, res) {
+//   res.render("signup");
+// };
 
-// For sign in
-exports.signin = function(req, res) {
-  res.render("signin");
-};
+// // For sign in
+// exports.signin = function(req, res) {
+//   res.render("signin");
+// };
 
-// For dashboard
-exports.dashboard = function(req, res) {
-  res.render("dashboard");
-};
+// // For dashboard
+// exports.dashboard = function(req, res) {
+//   res.render("dashboard");
+// };
 
-// For Logout
-exports.logout = function(req, res){
-  req.session.destroy(function(err){
-    es.redirect("/");
-  });
-};
+// // For Logout
+// exports.logout = function(req, res){
+//   req.session.destroy(function(err){
+//     es.redirect("/");
+//   });
+// };
