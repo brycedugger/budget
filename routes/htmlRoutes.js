f10233672def67d8c4b3f99673fea5ff3b367b1d
John <doanja@plu.edu>
2019-12-15 12:08:38 -0800

    fixed issue where wrong category id was being sent to the modal, added functions to delete expense, WIP: creating functionality to create expenses and categories

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,6 @@ module.exports = function(app) {
   // Load index page
   app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/HTML/test.html"));
-    // res.send('hello')
   });
 
   // // Render 404 page for any unmatched routes
