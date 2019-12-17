771030d5d91696268a4935ff7018b1ed10175f71
John Doan <doanja@plu.edu>
2019-12-17 13:52:54 -0800

    created scripts to render nav links

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,4 +1,14 @@
-const renderNavLinks = (title, userId, parentElement) => {
-  const li = $("<li>", { href: `/dashboard/${userId}` }).text(title);
+const renderNavLinks = (title, page, userId, parentElement) => {
+  const li = $("<li>", { class: "tab" });
+  const a = $("<a>", { href: `/${page}/${userId}` }).text(title);
   $(parentElement).append(li);
+  li.append(a);
 };
+
+$(document).ready(() => {
+  const userId = window.location.href.split("/")[window.location.href.split("/").length - 1];
+  console.log("userId :", userId);
+  renderNavLinks("Home", "dashboard", userId, ".tabs");
+  renderNavLinks("Expenses", "expenses", userId, ".tabs");
+  renderNavLinks("Budget", "budget", userId, ".tabs");
+});
