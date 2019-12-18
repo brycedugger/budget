dd5c47e1ca75ab79a3fa04b294ac06b89e3ca32a
John Doan <doanja@plu.edu>
2019-12-18 10:49:55 -0800

    fixed up dashboard.html nav links, updated profile page: submit button is rendered and jsdocs

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,12 +1,38 @@
+/**
+ * function to update the user's information
+ * @param {number} userId the user's id
+ * @param {string} firstName the user's first name
+ * @param {string} lastName the user's last name
+ * @param {string} email the user's email
+ */
 const updateUser = (userId, firstName, lastName, email) => {
   axios.put(`/api/user/${userId}`, { firstName, lastName, email }).then(res => {
-    console.log("posted");
+    location.reload();
   }),
     err => {
       console.log(err);
     };
 };
 
+/**
+ *
+ * @param {number} userId the user's id
+ */
+const getUserInfo = userId => {
+  axios.get(`/api/user/${userId}`).then(res => {
+    renderFormField("First name:", "text", "fname", res.data.firstName);
+    renderFormField("Last name:", "text", "lname", res.data.lastName);
+    renderFormField("Email:", "text", "email", res.data.email);
+    renderSubmitButton();
+  }),
+    err => {
+      console.log(err);
+    };
+};
+
+/**
+ * function to parse the form and update the uesr's information
+ */
 const parseFormData = () => {
   const userId = parseInt(
     window.location.href.split("/")[window.location.href.split("/").length - 1]
@@ -19,19 +45,26 @@ const parseFormData = () => {
   updateUser(userId, firstName, lastName, email);
 };
 
-const getUserInfo = userId => {
-  axios.get(`/api/user/${userId}`).then(res => {
-    renderFormField("First name:", "text", "fname", res.data.firstName);
-    renderFormField("Last name:", "text", "lname", res.data.lastName);
-    renderFormField("Email:", "text", "email", res.data.email);
-    // renderFormField("Password:", "password", "pwd", res.data.pwd);
-  }),
-    err => {
-      console.log(err);
-    };
+// function to render the submit button
+const renderSubmitButton = () => {
+  // create html elements
+  const formGroup = $("<div>", { class: "form-group" });
+  const col = $("<div>", { class: "col-lg-8" });
+  const input = $("<input>", {
+    type: "button",
+    class: "btn btn-primary",
+    value: "Submit",
+    id: "submit-button"
+  });
+
+  // append and render html elements
+  $("#form").append(formGroup);
+  formGroup.append(col);
+  col.append(input);
 };
 
 const renderFormField = (text, type, valueType, value) => {
+  // create html elements
   const formGroup = $("<div>", { class: "form-group" });
   const label = $("<label>", { class: "col-lg-3 control-label" }).text(text);
   const col = $("<div>", { class: "col-lg-8" });
@@ -39,6 +72,7 @@ const renderFormField = (text, type, valueType, value) => {
     value
   );
 
+  // append and render html elements
   $("#form").append(formGroup);
   formGroup.append(label, col);
   col.append(input);
@@ -48,7 +82,10 @@ $(document).ready(function() {
   const userId = parseInt(
     window.location.href.split("/")[window.location.href.split("/").length - 1]
   );
+
+  // get the user's information from the url
   getUserInfo(userId);
 
-  $(document).on("click", "#save-button", parseFormData);
+  //  listen for form submission
+  $(document).on("click", "#submit-button", parseFormData);
 });
