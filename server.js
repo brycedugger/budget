8406d81a6c7fdba78d2ebf8ab59865dca0d3fd74
John <spandexerjd@gmail.com>
2019-12-21 12:22:15 -0800

    added console log for dirname

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ if (process.env.NODE_ENV === "test") {
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
+    console.log("dirname:", __dirname);
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
