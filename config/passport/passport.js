cb6048ab382df2de9872a63a8106d33468183664
Bryce Dugger <bryce.dugger@outlook.com>
2020-01-02 20:48:37 -0800

    fixing error that had action of form to be /login not /signin

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -22,13 +22,13 @@ passport.use(
         // If there's no user with the given email
         if (!dbUser) {
           return done(null, false, {
-            message: "Incorrect email."
+            message: "Incorrect Username."
           });
         }
         // If there is a user with the given email, but the password the user gives us is incorrect
         else if (!dbUser.validPassword(password)) {
           return done(null, false, {
-            message: "Incorrect password."
+            message: "Incorrect Password."
           });
         }
         // If none of the above, return the user
